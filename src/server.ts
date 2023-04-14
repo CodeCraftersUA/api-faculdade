cc3aba467b7c7cb046381ec9c50450a81f104e74
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2023-04-14 06:13:32 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+// Dependencies
+import express from "express";
+import dotenv from "dotenv";
+import "express-async-errors";
+
+dotenv.config(); // Config dotenv
+const PORT = process.env.PORT || 3000; // API listen port
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const start = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`API running at: http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit();
+  }
+};
+start();
