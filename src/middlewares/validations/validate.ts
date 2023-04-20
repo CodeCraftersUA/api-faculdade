e749e2a710ba7719214423026290ea9b4a51d110
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2023-04-20 19:38:30 -0300

    feat: implement yup validations on course routes

---
+++ src/middlewares/validations/validate.ts
@@ -0,0 +1,29 @@
+// Dependencies
+import { NextFunction, Request, Response } from "express";
+import { Schema } from "yup";
+
+// Error class
+import AppError from "../../errors/AppError.js";
+
+const validate = (schema: Schema) => async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await schema.validate({
+      body: req.body,
+      query: req.query,
+      params: req.params
+    });
+
+    return next();
+  } catch (err) {
+    if (err.path)
+      throw new AppError(`${err.path} is invalid`);
+
+    throw err;
+  }
+};
+
+export default validate;
--- src/routes/coursesRoutes.ts
+++ src/routes/coursesRoutes.ts
@@ -8,6 +8,10 @@ import GetCourseController from "../modules/courses/useCases/getCourse/GetCourse
 import ListCoursesController from "../modules/courses/useCases/listCourses/ListCoursesController.js";
 import UpdateCourseController from "../modules/courses/useCases/updateCourse/UpdateCourseController.js";
 
+// Middlewares
+import postCourseValidate from "../middlewares/validations/courses/postCourseValidation.js";
+import putCourseValidate from "../middlewares/validations/courses/putCourseValidation.js";
+
 const app = express();
 
 const createCourseController = new CreateCourseController();
@@ -18,8 +22,8 @@ const updateCourseController = new UpdateCourseController();
 
 app.get("", listCourseController.handler);
 app.get("/:id", getCourseController.handler);
-app.post("", createCourseController.handler);
-app.put("/:id", updateCourseController.handler);
+app.post("", postCourseValidate, createCourseController.handler);
+app.put("/:id", putCourseValidate, updateCourseController.handler);
 app.delete("/:id", deleteCourseController.handler);
 
 export default app;
