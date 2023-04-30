c691d8ceb64e6689239d05d8a6b7ee9d69350c4a
Luiz H <luizhenriquealvesrodrigue64@gmail.com>
2023-04-30 17:55:40 -0300

    feat: filter courses by params

---
+++ src/modules/courses/useCases/listCourses/ListCoursesByParamsUseCase.ts
@@ -0,0 +1,27 @@
+// Dependencies
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class GetCoursesByParamsUseCase {
+	execute = async (
+		{
+			name,
+			acronym
+		}: {
+			name?: string,
+			acronym?: string
+		}
+	) => {
+		const course = await prisma.course.findMany({
+			where: {
+				name,
+				acronym
+			}
+		});
+
+		return course;
+	};
+}
+
+export default GetCoursesByParamsUseCase;
