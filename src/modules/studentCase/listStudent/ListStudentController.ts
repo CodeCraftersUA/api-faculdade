42a511e506609123dfdc73654394506759090914
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-05-01 19:26:58 -0300

    feat: filter student by name

--- src/modules/studentCase/listStudent/ListStudentController.ts
+++ src/modules/studentCase/listStudent/ListStudentController.ts
@@ -1,11 +1,26 @@
+// Dependencias
 import { Request, Response } from "express";
+
+// UseCases
 import ListStudentUseCase from "./ListStudentUseCase.js";
+import ListStudentsByNameUseCase from "./ListStudentsByNameUseCase.js";
 
 class ListStudentController {
 	handler = async (req: Request, res: Response) => {
-		const listStudentUseCase = new ListStudentUseCase();
-		const students = await listStudentUseCase.execute();
 
+		const listStudentUseCase = new ListStudentUseCase();
+		const listStudentsByNameUseCase = new ListStudentsByNameUseCase();
+
+		const queryName = typeof req.query.name === "string" ? req.query.name : undefined;
+
+		if (req.query.name){
+			const student = await listStudentsByNameUseCase.execute({
+				name: queryName?.toUpperCase()
+			});
+			return res.status(200).json(student);
+		}
+
+		const students = await listStudentUseCase.execute();
 		res.status(200).json(students);
 	};
 }
