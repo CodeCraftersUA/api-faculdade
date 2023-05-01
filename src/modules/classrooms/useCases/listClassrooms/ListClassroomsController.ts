16f880e7d087d2ecfe0ec5377fd097055e2db821
Luiz H <luizhenriquealvesrodrigue64@gmail.com>
2023-05-01 19:17:43 -0300

    feat: filter classrooms by query

--- src/modules/classrooms/useCases/listClassrooms/ListClassroomsController.ts
+++ src/modules/classrooms/useCases/listClassrooms/ListClassroomsController.ts
@@ -3,10 +3,24 @@ import { Request, Response } from "express";
 
 // UseCases
 import ListClassroomsUseCase from "./ListClassroomsUseCase.js";
+import ListClassroomsByParamsUseCase from "./ListClassroomsByParamsUseCase.js";
 
 class ListCoursesController {
 	handler = async (req: Request, res: Response) => {
 		const listClassroomsUseCase = new ListClassroomsUseCase();
+		const listClassroomsByParamsUseCase = new ListClassroomsByParamsUseCase();
+
+		const queryYear = typeof req.query.year === "string" ? parseInt(req.query.year) : undefined;
+		const querySemester = typeof req.query.semester === "string" ? parseInt(req.query.semester) : undefined;
+
+		if (queryYear || querySemester) {
+			const classrooms = await listClassroomsByParamsUseCase.execute({
+				year: queryYear,
+				semester: querySemester
+			});
+
+			return res.status(200).json(classrooms);
+		}
 
 		const courses = await listClassroomsUseCase.execute();
 		res.status(200).json(courses);
