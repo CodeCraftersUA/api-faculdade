e78a965d67fafb9938e46eb44bae1a49b8a9fe50
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-05-02 15:50:55 -0300

    test: test courses

--- src/modules/courses/useCases/createCourse/CreateCourseController.ts
+++ src/modules/courses/useCases/createCourse/CreateCourseController.ts
@@ -7,9 +7,9 @@ import CreateCourseUseCase from "./CreateCourseUseCase.ts";
 class CreateCourseController {
 	handler = async (req: Request, res: Response) => {
 		const createCourseUseCase = new CreateCourseUseCase();
-		await createCourseUseCase.execute(req.body);
+		const newCourse = await createCourseUseCase.execute(req.body);
 
-		res.sendStatus(201);
+		res.status(201).json(newCourse);
 	};
 }
 
--- src/modules/courses/useCases/createCourse/CreateCourseUseCase.ts
+++ src/modules/courses/useCases/createCourse/CreateCourseUseCase.ts
@@ -10,23 +10,25 @@ import generateUniqueId from "../../../../helpers/generateUniqueId.ts";
 // Interfaces
 import AppError from "../../../../errors/AppError.ts";
 import CourseInterface from "../../../../models/Course.ts";
+import { json } from "stream/consumers";
 
 const prisma = new PrismaClient();
 
 class CreateCourseUseCase {
 	execute = async (course: CourseInterface) => {
 		try {
-			await prisma.course.create({
+			const newCourse = await prisma.course.create({
 				data: {
 					id: generateUniqueId(),
 					name: course.name,
 					acronym: course.acronym.toLocaleUpperCase()
 				}
 			});
+			return newCourse;
+
 		} catch (err) {
 			if (err.code === KEY_ALREADY_EXISTS)
 				throw new AppError(`Attribute ${err.meta.target} already exists`, 400);
-
 			throw err;
 		}
 	};
